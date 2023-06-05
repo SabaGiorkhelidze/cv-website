5003bd954843b7e720b2453d217985adfb7c629a
SavageSB <sabagiorkhelidze@gmail.com>
2023-06-05 19:39:07 +0400

    added footer icons and links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,41 @@
 import React from "react";
-import { BoxIconElement } from "boxicons";
-
 import { AiFillGithub } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center mx-5 md:flex-row md:mx-24 2xl:mx-80  border-2 border-teal-900 py-10 ">
+      <div className="flex flex-col justify-center items-center mx-5 my-3 md:mx-24 2xl:mx-80 py-10 ">
         {/* mail contact */}
         <div className="flex justify-center items-center flex-col px-10">
           <h1 className="font-bold text-xl md:text-3xl">Get in Touch</h1>
         </div>
         {/* icons and links */}
-        <div className="grid grid-cols-2 gap-2">
-            <AiFillGithub />
+        <div className="grid grid-cols-2 gap-2 md:mt-4 md:flex md:flex-row md:gap-8">
+          <a
+            href="https://github.com/SavageSB?tab=repositories"
+            target="_blank"
+          >
+            <AiFillGithub className="text-3xl cursor-pointer text-cyan-600 hover:scale-110" />
+          </a>
+          <a href="https://www.facebook.com/saba.giorxelidze" target="_blank">
+            <AiFillFacebook className="text-3xl cursor-pointer text-cyan-600 hover:scale-110" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/saba-giorkhelidze-871ba3230/"
+            target="_blank"
+          >
+            <AiFillLinkedin className="text-3xl cursor-pointer text-cyan-600 hover:scale-110" />
+          </a>
+          <a
+            href="https://www.instagram.com/saba.giorkhelidze/"
+            target="_blank"
+          >
+            {" "}
+            <AiOutlineInstagram className="text-3xl cursor-pointer text-cyan-600 hover:scale-110" />
+          </a>
         </div>
       </div>
     </>
